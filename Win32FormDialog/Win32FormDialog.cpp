0b223f77558e2f6e59c4767e1b6993b0e75d28b2
limhoontaig <limhoontaig@gmail.com>
2024-05-03 20:55:02 +0900

    fileopen and save

--- Win32FormDialog/Win32FormDialog.cpp
+++ Win32FormDialog/Win32FormDialog.cpp
@@ -1,7 +1,11 @@
-﻿// Win32FormDialog.cpp : 애플리케이션에 대한 진입점을 정의합니다.
+﻿// win32Dialog.cpp : 애플리케이션에 대한 진입점을 정의합니다.
 //
 
+
+#include <stdio.h>
 //#include <commdlg.h>
+#include "resource.h"
+#include <windows.h>
 #include "framework.h"
 #include "Win32FormDialog.h"
 
@@ -11,21 +15,233 @@
 HINSTANCE hInst;                                // 현재 인스턴스입니다.
 WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
 WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HWND  g_hModelessWnd; 
+
 
 // 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    DlgProc(HWND, UINT, WPARAM, LPARAM);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
-    DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
-    return 0;
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: 여기에 코드를 입력합니다.
+    /*
+    {
+        DialogBox(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc);
+        return 0;
+    }
+    */
+    // 전역 문자열을 초기화합니다.
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_WIN32FORMDIALOG, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // 애플리케이션 초기화를 수행합니다:
+    if (!InitInstance(hInstance, nCmdShow))
+    {
+        return FALSE;
+    }
+
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32FORMDIALOG));
+
+    MSG msg;
+
+    // 기본 메시지 루프입니다:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int)msg.wParam;
 }
 
 
+
+//
+//  함수: MyRegisterClass()
+//
+//  용도: 창 클래스를 등록합니다.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WIN32FORMDIALOG));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WIN32FORMDIALOG);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   함수: InitInstance(HINSTANCE, int)
+//
+//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//
+//   주석:
+//
+//        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
+//        주 프로그램 창을 만든 다음 표시합니다.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+    if (!hWnd)
+    {
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE;
+}
+
+//
+//  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  용도: 주 창의 메시지를 처리합니다.
+//
+//  WM_COMMAND  - 애플리케이션 메뉴를 처리합니다.
+//  WM_PAINT    - 주 창을 그립니다.
+//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    int wmId, wmEvent;
+    PAINTSTRUCT ps;
+    HDC hdc;
+
+    OPENFILENAME ofn;
+    static char strFileTitle[MAX_PATH];
+    static char strFile[100];
+    static char strFileExtension[10];
+    char string[100];
+
+    switch (message)
+    {
+    case WM_COMMAND:
+    {
+        wmId = LOWORD(wParam);
+        wmEvent = HIWORD(wParam);
+        // 메뉴 선택을 구문 분석합니다:
+        switch (wmId)
+        {
+        case ID_32771:
+            memset(&ofn, 0, sizeof(OPENFILENAME));
+            ofn.lStructSize = sizeof(OPENFILENAME);
+            ofn.hwndOwner = hWnd;
+            ofn.lpstrTitle = "파일을 선택해 주세요.";
+            ofn.lpstrFileTitle = strFileTitle;
+            ofn.lpstrFile = strFile;
+            ofn.lpstrFilter = "비트맵(*.bmp)\0*.bmp\0jpg 파일\0*.jpg\0";
+            ofn.nMaxFile = MAX_PATH;
+            ofn.nMaxFileTitle = MAX_PATH;
+            if (GetOpenFileName(&ofn) != 0)
+            {
+                switch (ofn.nFilterIndex)
+                {
+                case 1: sprintf_s(strFileExtension, "%s", "bmp");
+                    break;
+                case 2 : sprintf_s(strFileExtension, "%s", "jpg");
+                    break;
+                }
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+            break;
+        case ID_32772:
+            memset(&ofn, 0, sizeof(OPENFILENAME));
+            ofn.lStructSize = sizeof(OPENFILENAME);
+            ofn.hwndOwner = hWnd;
+            ofn.lpstrTitle = "저장할 파일을 입력해 주세요.";
+            ofn.lpstrFileTitle = strFileTitle;
+            ofn.lpstrFile = strFile;
+            ofn.lpstrFilter = "비트맵(*.bmp)\0*.bmp\0jpg 파일\0*.jpg\0";
+            ofn.nMaxFile = MAX_PATH;
+            ofn.nMaxFileTitle = MAX_PATH;
+            if (GetSaveFileName(&ofn) != 0)
+            {
+                switch (ofn.nFilterIndex)
+                {
+                case 1: sprintf_s(strFileExtension, "%s", "bmp");
+                    if (ofn.nFileExtension == 0)
+                    {
+                        strcat_s(strFileTitle, ".bmp");
+                        strcat_s(strFile, ".bmp");
+                    }
+                    break;
+                case 2: sprintf_s(strFileExtension, "%s", "jpg");
+                    if (ofn.nFileExtension == 0)
+                    {
+                        strcat_s(strFileTitle, ".jpg");
+                        strcat_s(strFile, ".jpg");
+                    }
+                    break;
+                }
+                InvalidateRect(hWnd, NULL, TRUE);
+            }
+            break;
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+        sprintf_s(string, "전체경로 : %s", strFile);
+        TextOut(hdc, 10, 10, string, strlen(string));
+        sprintf_s(string, "파 일 명 : %s", strFileTitle);
+        TextOut(hdc, 10, 25, string, strlen(string));
+        sprintf_s(string, "확 장 자 : %s", strFileExtension);
+        TextOut(hdc, 10, 40, string, strlen(string));
+        EndPaint(hWnd, &ps);
+    }
+    break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
 
 // 정보 대화 상자의 메시지 처리기입니다.
 INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
@@ -48,6 +264,28 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         EndDialog(hDlg, 0);
         (INT_PTR)TRUE;
 
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+
+// 정보 대화 상자의 메시지 처리기입니다.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
     }
     return (INT_PTR)FALSE;
 }
