84f5adbd9c49ce2a7d8e5e3d7d80c06b4d8e071c
limhoontaig <limhoontaig@gmail.com>
2024-05-06 08:57:11 +0900

    List Control click function

--- win32ListControl/resource.h
+++ win32ListControl/resource.h
@@ -15,6 +15,10 @@
 #define IDD_DIALOG1                     129
 #define IDC_LIST1                       1000
 #define IDC_LIST2                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_EDIT1                       1003
+#define IDC_EDIT2                       1004
+#define IDC_EDIT3                       1005
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -24,7 +28,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
--- win32ListControl/win32ListControl.cpp
+++ win32ListControl/win32ListControl.cpp
@@ -34,11 +34,15 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     static HWND hList;
     char* strMenu[] = { "항목 1", "항목 2", "항목 3", "항목 4"};
     LVCOLUMN lvColumn;
+    LVITEM lvItem;
+    static char string[100];
+    int nIndex;
 
     switch (message)
     {
     case WM_INITDIALOG:
         hList = GetDlgItem(hDlg, IDC_LIST1);
+        ListView_SetExtendedListViewStyle(hList, LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
         lvColumn.mask = LVCF_FMT | LVCF_WIDTH | LVCF_TEXT | LVCF_SUBITEM ;
         lvColumn.fmt = LVCFMT_CENTER;
         for (int i = 0; i < 4; i++)
@@ -50,10 +54,53 @@ INT_PTR CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
         return (INT_PTR)TRUE;
 
     case WM_COMMAND:
+        switch (LOWORD(wParam))
+        {
+        case IDC_BUTTON1: // ListView Insert Item
+
+            // ListView 초기화 및 Index 생성
+            lvItem.iItem = ListView_GetItemCount(hList);
+            lvItem.iSubItem = 0;
+            lvItem.mask = LVIF_TEXT;
+            sprintf_s(string, "%d", lvItem.iItem);
+            lvItem.pszText = string;
+            ListView_InsertItem(hList, &lvItem);
+            
+            // ListView SubItem Input
+            GetDlgItemText(hDlg, IDC_EDIT1, string, 10);
+            ListView_SetItemText(hList, lvItem.iItem, 1, string);
+            GetDlgItemText(hDlg, IDC_EDIT2, string, 10);
+            ListView_SetItemText(hList, lvItem.iItem, 2, string);
+            GetDlgItemText(hDlg, IDC_EDIT3, string, 10);
+            ListView_SetItemText(hList, lvItem.iItem, 3, string);
+            
+            // 데이터 입력용 EditControl 데이터 클리어
+            SetDlgItemText(hDlg, IDC_EDIT1, NULL);
+            SetDlgItemText(hDlg, IDC_EDIT2, NULL);
+            SetDlgItemText(hDlg, IDC_EDIT3, NULL);
+            return (INT_PTR)TRUE;
+        }
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
         {
             EndDialog(hDlg, LOWORD(wParam));
             return (INT_PTR)TRUE;
+        }
+        break;
+    case WM_NOTIFY:
+        switch (((LPNMHDR)lParam)->code)
+        {
+        case NM_CLICK:
+            nIndex = ListView_GetNextItem(hList, -1, LVNI_SELECTED);
+            // sprintf_s(string, "Selected Item is %d", nIndex);
+
+            ListView_GetItemText(hList, nIndex, 1, string, 10);
+            //MessageBox(hDlg, string, "NM_CLICK", MB_YESNO);
+            SetDlgItemText(hDlg, IDC_EDIT1, string);
+            ListView_GetItemText(hList, nIndex, 2, string, 10);
+            SetDlgItemText(hDlg, IDC_EDIT2, string);
+            ListView_GetItemText(hList, nIndex, 3, string, 10);
+            SetDlgItemText(hDlg, IDC_EDIT3, string);
+            return TRUE;
         }
         break;
     }
