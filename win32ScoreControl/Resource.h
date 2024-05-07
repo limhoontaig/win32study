3d15969eee9b61a69f3675e86e9f7a5f6c9e7962
limhoontaig <=>
2024-05-07 11:42:45 +0900

    create Login window and main window

--- win32ScoreControl/Resource.h
+++ win32ScoreControl/Resource.h
@@ -13,6 +13,9 @@
 #define IDC_WIN32SCORECONTROL           109
 #define IDR_MAINFRAME                   128
 #define IDD_DIALOG1                     129
+#define IDD_DIALOG2                     130
+#define IDC_EDIT1                       1000
+#define IDC_EDIT2                       1001
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -20,9 +23,9 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1002
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
