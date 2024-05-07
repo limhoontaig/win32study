5c544bec6095620ed2d02569ebedcbfeb10a1068
limhoontaig <limhoontaig@gmail.com>
2024-05-08 07:43:53 +0900

    menu ID_32771 create

--- win32ScoreControl/Resource.h
+++ win32ScoreControl/Resource.h
@@ -20,6 +20,7 @@
 #define IDC_EDIT3                       1002
 #define IDC_EDIT4                       1003
 #define IDC_EDIT5                       1004
+#define ID_32771                        32771
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -28,7 +29,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_COMMAND_VALUE         32772
 #define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
