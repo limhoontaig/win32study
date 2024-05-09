06a2220e7a862c503145243cca7f096b17fb907a
limhoontaig <limhoontaig@gmail.com>
2024-05-09 20:10:29 +0900

    text file numbler of lines

--- c_fileInOut/c_fileInOut.c
+++ c_fileInOut/c_fileInOut.c
@@ -1,6 +1,4 @@
-﻿// c_fileInOut.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
-
+﻿// fopen 등 사용을 위한 에러 방지 
 #pragma warning(disable : 4996)
 
 #include<stdio.h>
@@ -18,73 +16,28 @@ int main()
 	char buf[100];
 	int len;
 	int count = 0;
-	int num_Users;
+	int num_Users = 0;
 	int* pnum_Users = &num_Users;
 	
 	void NumberOfUsers(int* num_Users);
 
 	NumberOfUsers(&num_Users);
 	printf("Number of Users : %d\n\n", num_Users);
-	/*
-	if ((fp = fopen("E:\\win32Study\\win32ScoreControl\\secret.txt", "a")) == NULL) {
-		printf("파일열기 실패\n");
-	}
 	
-	
-	for (i = 0; i < 1; i++) 
-	{
-		printf("%d번째 유저정보를 입력 (아이디): \n", i);
-		scanf("%s",name);
-		printf("%d번째 유저정보를 입력 (비밀번호): \n", i);
-		scanf("%s", password);
-
-		fprintf(fp, "%s %s \n", name, password);
-	}
-	fclose(fp);
-	*/
-	fp = fopen("E:\\win32Study\\win32ScoreControl\\secret.txt", "r");
-	if (fp  == NULL) {
-		printf("파일열기 실패\n");
-	}
-	/*
-	while (1) 
-	{
-		fgets(buf, 100, fp);
-		len = strlen(buf);
-		if (len == 0)
-			break;
-		fscanf(fp, "%d %s %s", &i, name, password);
-		printf("while No %d name %s, password %s\n", i, name, password);
-		count++;
-		buf[0] = NULL;
-	}
-	
-	fclose(fp);
-	*/
 	fp = fopen("E:\\win32Study\\win32ScoreControl\\secret.txt", "r");
 	if (fp == NULL) {
-		printf("파일열기 실패\n");
+		fp = fopen("C:\\Users\\82109\\source\\repos\\limhoontaig\\win32study\\win32ScoreControl\\secret.txt", "r");
+		if (fp == NULL) 
+			printf("파일열기 실패\n");
 	}
 	
-	// for (i = 0; i < count; i++) {
-	while(!feof(fp))
+	for (int i = 0; i < num_Users; i++)
 	{
-		
 		fscanf(fp, "%d %s %s", &age, name, password);
 		printf("No %d, 순번 %d, name %s, password %s\n", i, age, name, password );
-		//ave_age += (double)age;
 	}
-
-	//printf("유저들의 평균나이 : %0.2lf\n", ave_age / 5);
-	/*
-	fseek(fp, 0, SEEK_END);
-	nFileSize = ftell(fp);
-	printf("file size of user_info.text : %d", nFileSize);
-	*/
 	fclose(fp);
-	//}
 	return 0;
-
 }
 
 void NumberOfUsers(int* a) {
@@ -92,8 +45,11 @@ void NumberOfUsers(int* a) {
 	char buf[100];
 	int len;
 	int count= 0;
-	if ((fp = fopen("E:\\win32Study\\win32ScoreControl\\secret.txt", "r")) == NULL) {
-		printf("파일열기 실패\n");
+	fp = fopen("E:\\win32Study\\win32ScoreControl\\secret.txt", "r");
+	if (fp == NULL) {
+		fp = fopen("C:\\Users\\82109\\source\\repos\\limhoontaig\\win32study\\win32ScoreControl\\secret.txt", "r");
+		if (fp == NULL)
+			printf("파일열기 실패\n");
 	}
 	while (1)
 	{
@@ -101,20 +57,10 @@ void NumberOfUsers(int* a) {
 		len = strlen(buf);
 		if (len == 0)
 			break;
-		*a = count++;
+		*a = ++count;
 		buf[0] = NULL;
 	}
-
 	fclose(fp);
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
